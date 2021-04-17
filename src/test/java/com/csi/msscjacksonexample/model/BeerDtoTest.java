188a0777ab1209737209e0a6461702bcbd11b4b9
prajul-shinde <prajulshinde@gmail.com>
2021-04-17 18:09:02 +0530

    Custom Deserializer to format a Date

--- src/test/java/com/csi/msscjacksonexample/model/BeerDtoTest.java
+++ src/test/java/com/csi/msscjacksonexample/model/BeerDtoTest.java
@@ -6,25 +6,24 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @JsonTest
-class BeerDtoTest extends BaseTest{
+class BeerDtoTest extends BaseTest {
     @Autowired
     ObjectMapper objectMapper;
 
     @Test
-    //serializing to json
+        //serializing to json
     void testSerializeDto() throws JsonProcessingException {
-        BeerDto beerDto=getDto();
-        String jsonString=objectMapper.writeValueAsString(beerDto);
+        BeerDto beerDto = getDto();
+        String jsonString = objectMapper.writeValueAsString(beerDto);
         System.out.println(jsonString);
 
     }
+
     @Test
-    //json to object
+        //json to object
     void testDeserialize() throws JsonProcessingException {
-        String json="{\"beerId\":\"8ef68ea2-fde6-4fc3-a424-40fec4227559\",\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":123123123,\"price\":12.99,\"createdDate\":\"2021-04-17T16:23:08.7738288+05:30\",\"lastUpdatedDate\":\"2021-04-17T16:23:08.7738288+05:30\"}\n";
+        String json = "{\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":123123123,\"price\":\"12.99\",\"createdDate\":\"2021-04-17T18:06:58+0530\",\"lastUpdatedDate\":\"2021-04-17T18:06:58.8236075+05:30\",\"myLocalDate\":\"20210417\",\"beerId\":\"7a5a0986-413d-45a7-83b1-b771b1503b00\"}\n";
         BeerDto dto = objectMapper.readValue(json, BeerDto.class);
         System.out.println(dto);
     }
